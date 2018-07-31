4ee5ecc4bab27329cd837afc36e2214cabcf58e2
massa <massafranck@gmail.com>
2018-07-31 15:07:52 +0200

    initial

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8" />
+  <title>Exercice4</title>
+</head>
+<body>
+  <p>
+    <?php
+    //Teste que les paramètres de l'url existent
+    if(isset($_GET['language']) && isset($_GET['server'])){
+      echo $_GET['language'] . ' ' . $_GET['server'];
+    }
+    ?>
+  </p>
+</body>
+</html>
