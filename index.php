2eaac41ca344b2283a3aa6015312f004f024c0c8
massa <massafranck@gmail.com>
2018-08-20 22:21:06 +0200

    corrigé

--- index.php
+++ index.php
@@ -1,17 +1,22 @@
 <!DOCTYPE html>
 <html lang="fr">
-<head>
-  <meta charset="utf-8" />
-  <title>Exercice4</title>
-</head>
-<body>
-  <p>
-    <?php
-    //Teste que les paramètres de l'url existent et affichage des valeurs
-    if(isset($_GET['language']) && isset($_GET['server'])){
-      echo $_GET['language'] . ' ' . $_GET['server'];
-    }
-    ?>
-  </p>
+    <head>
+        <meta charset="utf-8" />
+        <title>exercice4</title>
+    </head>
+    <body>
+        <?php
+        if (!empty($_GET['language'])){
+            echo $_GET['language'] . ' ';
+        } else {
+            echo 'Il manque le paramètre language ';
+        }
+        if (!empty($_GET['server'])){
+            echo $_GET['server'];
+        } else {
+            echo 'Il manque le paramètre server';
+        }
+        ?>
+    </p>
 </body>
 </html>
